0ba49ef63e186c7a785554b7df632585711d1501
Danilo <fixmyprinter@danilorechi.net>
2016-10-04 14:44:59 -0300

    visual fix

--- index.js
+++ index.js
@@ -18,13 +18,19 @@ let win;
 
 function createWindow () {
 
-  win = new BrowserWindow({width: 800, height: 600})
-  win.loadURL(`file://${__dirname}/index.html`)
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        fullscreen: true
+    });
 
-  // Close click, alt + f4 or program call
-  win.on('closed', () => {
-    win = null
-  })
+    win.setMenuBarVisibility(false);
+    win.loadURL(`file://${__dirname}/index.html`)
+
+    // Close click, alt + f4 or program call
+    win.on('closed', () => {
+        win = null
+    })
 }
 
 // Electron is ready
@@ -32,15 +38,15 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  if (win === null) {
-    createWindow()
-  }
+    if (win === null) {
+        createWindow()
+    }
 })
 
 ipcMain.on('move', (event, direction)=> {
